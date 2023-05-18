85b8ad137f5fa4753b6fb7078fed989f55fa1436
Powerinno <wolfy_alex10@yahoo.com>
2023-05-18 16:22:46 +0300

    change action script

--- action.ps1
+++ action.ps1
@@ -5,3 +5,6 @@ param(
 $split_out = $EVENTDATA.EventProperties.Message.Split(" ")
 Write-Output($split_out[0])
 Write-Output($split_out[1])
+
+$vm = Get-AzVM -ResourceGroupName "thunder" -Name "random-vm"
+Write-Output($vm)
