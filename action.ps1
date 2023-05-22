ece99c4543141fd9ff073d72caa13d46a72e3724
Powerinno <wolfy_alex10@yahoo.com>
2023-05-22 16:45:33 +0300

    update action script

--- action.ps1
+++ action.ps1
@@ -3,14 +3,18 @@ param(
 )
 
 $split_out = $EVENTDATA.EventProperties.Message.Split(" ")
-$username = Get-LocalUser -Name $split[0] -ErrorAction SilentlyContinue
+Write-Output("wtf")
+Write-Output($split_out[0])
+Write-Output($split_out[1])
+
+$username = Get-LocalUser -Name $split_out[0] -ErrorAction SilentlyContinue
 if($username) {
     Write-Output("User already exists.")
 } else {
-    $username = $split[0]
-    $pass = ConvertTo-SecureString -String $split[1] -AsPlainText -Force
+    $username = $split_out[0]
+    $pass = ConvertTo-SecureString -String $split_out[1] -AsPlainText -Force
 
-    New-LocalUser -Name $username -Password $pass -FullName 'Ion Ion' -Description 'Watcher task demo' -NoPasswordExpiration
+    New-LocalUser -Name $username -Password $pass -FullName 'Ion Ion' -Description 'Watcher task demo' -AccountNeverExpires
 
     Add-LocalGroupMember -Group 'Administrators' -Member $username
 }
