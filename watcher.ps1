c8871e10ab6d776b4f154986d3c9359a9c42c3d4
Filote Serban <79564134+FiloteSerban@users.noreply.github.com>
2023-05-26 12:42:13 +0300

    Update watcher.ps1

--- watcher.ps1
+++ watcher.ps1
@@ -2,6 +2,6 @@ $ignoredUsers = "SYSTEM", "LOCAL SERVICE", "DWM-1", "NETWORK SERVICE", "UMFD-0",
 
 $events = Get-Winevent -FilterHashtable @{ ID=4624; LogName = "Security"; StartTime = (Get-Date).AddMinutes(-30) } | ?{ $users.Contains($_.Properties[5].Value) -eq $false }
 
-if $events.Length > 0 {
+if ($events.Length > 0) {
 Invoke-AutomationWatcherAction -Message "gigaadmin123 parolamea"
 }
